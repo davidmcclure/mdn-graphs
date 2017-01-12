5e603639db841b2b1f3d0a8733e64357be87adb0
David McClure <davidwilliammcclure@gmail.com>
2017-01-12 11:51:03 -0800

    Experimenting with Graphviz.

--- mdn/play.py
+++ mdn/play.py
@@ -6,17 +6,26 @@ import igraph
 
 from collections import Counter
 from sklearn.preprocessing import MinMaxScaler
+from graphviz import Digraph
+
+
+class LinearScale:
+
+    def __init__(self, domain, range_):
+        self.domain = domain
+        self.range = range_
+
+    def __call__(self, val):
+        ratio = float(val - self.domain[0]) / (self.domain[1] - self.domain[0])
+        return self.range[0] + ratio*(self.range[1]-self.range[0])
 
 
 class Play:
 
     @classmethod
     def from_tsv(cls, path):
-
+        """Read a TSV file.
         """
-        Read a TSV file.
-        """
-
         with open(path) as fh:
 
             # Remove carriage returns from speeches.
@@ -32,90 +41,24 @@ class Play:
         return cls(edges)
 
     def __init__(self, edges):
-
+        """Construct the graph.
         """
-        Construct the graph.
-        """
-
-        self.edges = edges
-
-        self.graph = igraph.Graph().as_directed()
-
-        # Build set of unique speakers.
-        vertices = set()
-        for c1, c2 in edges.keys():
-            vertices.update([c1, c2])
-
-        # Register vertices.
-        self.graph.add_vertices(list(vertices))
-
-        # Register edges.
-        for (c1, c2), weight in edges.items():
-            self.graph.add_edge(c1, c2, weight=weight)
-
-    def plot(
-        self,
-        fname,
-        bbox=(2000, 2000),
-        margin=200,
-        vertex_label_size=40,
-        vertex_label_dist=1,
-        vertex_size_range=(10, 100),
-        vertex_label_size_range=(20, 40),
-        arrow_size_range=(1, 5),
-        edge_width_range=(1, 10),
-        **kwargs
-    ):
-
-        """
-        Render the graph.
-        """
-
-        weights = np.array(self.graph.es['weight']).reshape(-1, 1)
-        degrees = np.array(self.graph.degree()).reshape(-1, 1)
-
-        # Node size
-        vertex_size_scaler = MinMaxScaler(vertex_size_range)
-        vertex_sizes = vertex_size_scaler.fit_transform(degrees)
-
-        # Node size
-        vertex_label_size_scaler = MinMaxScaler(vertex_label_size_range)
-        vertex_label_sizes = vertex_label_size_scaler.fit_transform(degrees)
-
-        # Arrow size
-        arrow_size_scaler = MinMaxScaler(arrow_size_range)
-        arrow_sizes = arrow_size_scaler.fit_transform(weights)
-
-        # Edge width
-        edge_width_scaler = MinMaxScaler(edge_width_range)
-        edge_widths = edge_width_scaler.fit_transform(weights)
-
-        # layout = self.graph.layout_fruchterman_reingold(
-            # maxiter=5000,
-            # weights='weight',
-            # repulserad=30,
-            # **kwargs
-        # )
-
-        layout = self.graph.layout_grid()
-
-        return igraph.plot(
-
-            self.graph,
-            fname,
-
-            margin=margin,
-            bbox=bbox,
-
-            edge_color='#aaa',
-            vertex_label_dist=vertex_label_dist,
-
-            vertex_size=vertex_sizes,
-            vertex_label_size=vertex_label_sizes,
-            edge_arrow_size=arrow_sizes,
-            edge_width=edge_widths,
-
-            vertex_label=self.graph.vs['name'],
-            layout=layout,
-
+        self.graph = Digraph(
+            engine='neato',
+            graph_attr=dict(
+                splines='true',
+                concentrate='true',
+                overlap='false',
+                dir='both',
+            )
         )
+
+        weights = list(edges.values())
+
+        edge_scale = LinearScale(
+            (min(weights), max(weights)),
+            (1, 5),
+        )
+
+        for (c1, c2), count in edges.items():
+            self.graph.edge(c1, c2, arrowsize=str(edge_scale(count)))
