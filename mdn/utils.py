5042cb8b2f1c61593d18a345c247f978cc98b207
David McClure <davidwilliammcclure@gmail.com>
2017-01-18 14:30:00 -0800

    Add a LinearScale class.

--- mdn/utils.py
+++ mdn/utils.py
@@ -3,6 +3,7 @@
 import re
 import scandir
 import os
+import attr
 
 
 def scan_paths(root_dir, pattern):
@@ -21,3 +22,21 @@ def scan_paths(root_dir, pattern):
             # Match the extension.
             if pattern.search(name):
                 yield os.path.join(root, name)
+
+
+@attr.s
+class LinearScale(object):
+
+    d1 = attr.ib()
+    d2 = attr.ib()
+    r1 = attr.ib()
+    r2 = attr.ib()
+
+    def __call__(self, val):
+        """Map a value from domain interval onto the range.
+
+        Args:
+            val (int)
+        """
+        ratio = float(val-self.d1) / (self.d2-self.d1)
+        return self.r1 + ratio*(self.r2-self.r1)
