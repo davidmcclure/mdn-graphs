e9f4761ce5582aaa3346ea4d2389ee02d274e6dc
David McClure <davidwilliammcclure@gmail.com>
2017-01-26 20:12:04 -0800

    Stub in write_plays.

--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 
 
 import json
+import os
 
 from invoke import task
 
@@ -20,3 +21,25 @@ def write_paths(ctx):
 
     with open_makedirs('site/data/paths.json', 'w') as fh:
         json.dump(paths, fh)
+
+
+@task
+def write_plays(ctx):
+    """Write each play as JSON for d3.
+    """
+    paths = list(corpus.paths())
+
+    rel_paths = list(corpus.rel_paths())
+
+    for path, rel_path in zip(paths, rel_paths):
+
+        try:
+            play = Play.from_tsv(path)
+        except:
+            continue
+
+        out_path = os.path.join('site/data/plays', rel_path)
+
+        with open_makedirs(out_path, 'w') as fh:
+            json.dumps(play.as_d3_data(), fh)
+            print(rel_path)
